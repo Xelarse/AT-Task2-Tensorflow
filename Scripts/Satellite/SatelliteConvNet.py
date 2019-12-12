a3b7f7c48bec09c39f445eb59c794da5c0e403b3
Alex Allman <xalexxd@hotmail.co.uk>
2019-12-12 19:44:56 +0000

    Updated boi got the new badass models

--- Scripts/Satellite/SatelliteConvNet.py
+++ Scripts/Satellite/SatelliteConvNet.py
@@ -10,9 +10,11 @@ import os
 
 export_model = True
 dense_layers = [1]
-layer_sizes = [128]
-conv_layers = [3]
-model_runs = 200
+dense_sizes = [256, 512]
+layer_sizes = [64, 128]
+conv_layers = [2]
+kern_sizes = [7]
+model_runs = [150, 200, 300]
 
 pathToScript = os.getcwd()
 
@@ -28,37 +30,39 @@ y = keras.utils.to_categorical(y, 8)
 for dense_layer in dense_layers:
     for layer_size in layer_sizes:
         for conv_layer in conv_layers:
-            NAME = f"Satellite-{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense-{model_runs}-Epochs-{int(time.time())}"
-            tensorboard = TensorBoard(log_dir=os.path.join(pathToScript, "logs\\{}".format(NAME)))
-            print(NAME)
+            for dense_size in dense_sizes:
+                for kern_size in kern_sizes:
+                    for epoc in model_runs:
+                        NAME = f"Satellite-{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense-{dense_size}-dense_size-{kern_size}-kernel_size-{epoc}-Epochs-{int(time.time())}"
+                        tensorboard = TensorBoard(log_dir=os.path.join(pathToScript, "logs\\{}".format(NAME)))
+                        print(NAME)
 
-            model = Sequential()
+                        model = Sequential()
 
-            model.add(Conv2D(int(layer_size * 0.5), (3, 3), input_shape=X.shape[1:]))
-            model.add(Activation("relu"))
-            model.add(MaxPooling2D(pool_size=(2, 2)))
-            model.add(Dropout(0.25))
+                        model.add(Conv2D(layer_size, (kern_size, kern_size), input_shape=X.shape[1:]))
+                        model.add(Activation("relu"))
+                        model.add(MaxPooling2D(pool_size=(2, 2)))
+                        model.add(Dropout(0.25))
 
-            # First conv layer must have input shape but every one after is the same
-            for l in range(conv_layer-1):
-                model.add(Conv2D(layer_size, (3, 3)))
-                model.add(Activation("relu"))
-                model.add(MaxPooling2D(pool_size=(2, 2)))
-                model.add(Dropout(0.25))
+                        # First conv layer must have input shape but every one after is the same
+                        for l in range((conv_layer-1)):
+                            model.add(Conv2D(layer_size, (kern_size, kern_size)))
+                            model.add(Activation("relu"))
+                            model.add(MaxPooling2D(pool_size=(2, 2)))
+                            model.add(Dropout(0.25))
 
-            # model.add(MaxPooling2D(pool_size=(2, 2)))
-            model.add(Flatten())
-            for l in range(dense_layer):
-                model.add(Dense(256))
-                model.add(Activation("relu"))
-                model.add(Dropout(0.5))
+                        model.add(Flatten())
+                        for l in range(dense_layer):
+                            model.add(Dense(dense_size))
+                            model.add(Activation("relu"))
+                            model.add(Dropout(0.5))
 
-            model.add(Dense(8))
-            model.add(Activation("softmax"))
+                        model.add(Dense(8))
+                        model.add(Activation("softmax"))
 
-            #### Compile and fit
-            model.compile(loss="categorical_crossentropy", optimizer="adadelta", metrics=["accuracy"])
-            model.fit(X, y, batch_size=32, epochs=model_runs, validation_split=0.15, callbacks=[tensorboard], shuffle=True)
+                        #### Compile and fit
+                        model.compile(loss="categorical_crossentropy", optimizer="adadelta", metrics=["accuracy"])
+                        model.fit(X, y, batch_size=32, epochs=epoc, validation_split=0.3, callbacks=[tensorboard], shuffle=True)
 
-            if(export_model):
-                model.save(f"models/Satellite-{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense-{model_runs}-epochs.h5")
+                        if(export_model):
+                            model.save(f"models/Satellite-{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense-{dense_size}-dense_size-{kern_size}-kernel_size-{epoc}-Epochs.h5")
